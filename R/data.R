2a389f2d8ec784a696ca0375705156cf29b71c05
Daniel E Gonzalez <ixa.dgonzalez80@gmail.com>
2022-05-19 11:43:07 -0500

    ajustes a data casas

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Pricios y caracteristicas de viviendas.
+#'
+#' Esta data contiene los precios y principales caraceristica de vienes inmuebles
+#'
+#' @format Una data con 54 filas y 10 variables:
+#' \describe{
+#'   \item{id}{consecutivo}
+#'   \item{precio}{precio de la vivienda en miles de U$D}
+#'   \item{imp.pred}{valor impuesto predial}
+#'   \item{num.ban}{número de baños}
+#'   \item{area.lot}{área del lote}
+#'   \item{area.con}{área construida}
+#'   \item{num.gar}{número de garajes}
+#'   \item{num.cua}{número de cuartos}
+#'   \item{num.hab}{número de habitaciones}
+#'   \item{edad}{edad de la vivienda}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"casas"
